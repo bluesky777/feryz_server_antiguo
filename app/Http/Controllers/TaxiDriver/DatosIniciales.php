3c18822ac22dad2ba6867b093c7210def01057ee
Joseth Guerrero <davidguerrero777@gmail.com>
2018-10-15 15:22:48 -0500

    una coma

--- app/Http/Controllers/TaxiDriver/DatosIniciales.php
+++ app/Http/Controllers/TaxiDriver/DatosIniciales.php
@@ -198,7 +198,7 @@ class DatosIniciales {
 					(taxi_id, taxista_id, zona, fecha_ini, lugar_inicio, lugar_fin, fecha_fin, estado, created_at, updated_at)
 				VALUES
 					(2,2,'z1','2018-07-22 16:26','el parque','el parque2','2018-07-22','Cancelada', '".$now."', '".$now."'),
-					(3,3,'z1','2018-07-22','los lanceros','el parque2','2018-07-22','Cancelada', '".$now."', '".$now."'),
+					(3,3,'z1','2018-07-22','los lanceros','el parque2','2018-07-22','Cancelada', '".$now."', '".$now."')
 					;";
 					
 			DB::insert($consulta);
